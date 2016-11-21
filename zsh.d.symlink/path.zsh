6ca65406202a0fbb06a7828e5abe2060f15da6d5
Shane Liesegang <shane@techie.net>
2016-11-20 21:36:28 -0800

    encryption and gopath

--- zsh.d.symlink/path.zsh
+++ zsh.d.symlink/path.zsh
@@ -1,9 +1,11 @@
 local pyPath=""
 if [[ $OSTYPE == darwin* ]]; then
   pyPath="$HOME/Library/Python/2.7/bin"
+  export GOPATH="$HOME/Projects/go"
 else
   # doesn't handle windows, but none of this stuff does :)
   pyPath="$HOME/.local/bin"
+  export GOPATH="$HOME/.local/go"
 fi
 
-export PATH="./bin:/usr/local/bin:/usr/local/sbin:$HOME/bin:$pyPath:$PATH"
+export PATH="./bin:/usr/local/bin:/usr/local/sbin:$HOME/bin:$pyPath:$GOPATH/bin:$PATH"
