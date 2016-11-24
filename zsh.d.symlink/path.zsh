048f8ba57e7a7669c8ee601f9997223310b6c0a9
Shane Liesegang <shane@techie.net>
2016-11-24 12:16:42 -0800

    Updating GOPATH to present standard

--- zsh.d.symlink/path.zsh
+++ zsh.d.symlink/path.zsh
@@ -1,5 +1,5 @@
 local pyPath="$(python -m site --user-base)/bin"
 local rbPath="$(ruby -rubygems -e 'puts Gem.user_dir')/bin"
 
-export GOPATH="$HOME/Projects/go_workspace"
+export GOPATH="$HOME/go"
 export PATH="$HOME/bin:$pyPath:$rbPath:$GOPATH/bin:/usr/local/bin:/usr/local/sbin:$PATH"
