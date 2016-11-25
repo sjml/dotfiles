836c77ddd117c24a81fc9b2d7e5cfb2c19b1e353
Shane Liesegang <shane@techie.net>
2016-11-25 14:39:39 -0500

    Brewfile updates

--- brewfile_audit.py
+++ brewfile_audit.py
@@ -1,8 +1,10 @@
+import os
 import sys
 import re
 import subprocess
 
 packages = []
+casks = []
 brewData = open("Brewfile", "r").readlines()
 for command in brewData:
     if (len(command.strip()) == 0):
@@ -12,11 +14,26 @@ for command in brewData:
     if (command.strip().startswith("brew '")):
         pkg = re.match(r"brew '([^']*)'", command)
         packages.append(pkg.group(1))
+    elif (command.strip().startswith("cask '")):
+        cask = re.match(r"cask '([^']*)'", command)
+        casks.append(cask.group(1))
 
 
 deps = subprocess.check_output(["/usr/local/bin/brew", "deps", "--union"] + packages)
 
+erred = False
 for dep in deps.split("\n"):
     if dep == "python":
         sys.stderr.write("WARNING: this Brewfile will result in Homebrew Python.\n")
-        sys.exit(1)
+        erred = True
+
+for cask in casks:
+    FNULL = open(os.devnull, "w")
+    infoStatus = subprocess.call(["brew", "cask", "info", cask], stdout=FNULL, stderr=FNULL)
+    if (infoStatus != 0):
+        sys.stderr.write("WARNING: problematic cask: %s\n" % cask)
+        erred = True
+
+if (erred):
+    sys.exit(1)
+
