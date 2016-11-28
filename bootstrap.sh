cc317b612a8ea3ef66760b9c9fb90454bb6e4680
Shane Liesegang <shane@techie.net>
2016-11-27 22:34:43 -0800

    pathing

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,7 +7,7 @@ cd $scriptPath
 curl -L https://api.github.com/repos/sjml/dotfiles/tarball > $scriptPath/dotfiles.tar.gz
 pathName=$(tar -ztvf dotfiles.tar.gz | head -1 | awk 'match($0,/sjml-dotfiles-([a-f0-9]*)\//) {print substr($0,RSTART,RLENGTH)}')
 tar -xzf dotfiles.tar.gz
-rm dotfiles.tar.gz
+rm $scriptPath/dotfiles.tar.gz
 mv $scriptPath/$pathName $scriptPath/.dotfiles
 
 cd $scriptPath/.dotfiles
