75e016c79455c06d2045985bca4cdd4b918f6b09
Shane Liesegang <shane@techie.net>
2017-02-15 19:22:15 -0800

    updating submodules

--- zsh.d.symlink/aliases.zsh
+++ zsh.d.symlink/aliases.zsh
@@ -14,7 +14,7 @@ else
 fi
 
 alias mkdir="mkdir -p"
-alias gitsubpull="git submodule update --recursive --remote"
+alias gitsubpull="git pull --recurse-submodules; git submodule update --recursive --remote"
 
 # Mac-specific aliases
 if [[ $OSTYPE == darwin* ]]; then
