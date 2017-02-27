b49119becc549fc1f52e4ba2378297e99c9bd7bc
Shane Liesegang <shane@techie.net>
2017-02-27 06:32:28 -0800

    updating angular name

--- provision-mac.sh
+++ provision-mac.sh
@@ -86,7 +86,7 @@ timerData "POST-PYTHON"
 # node setup
 zsh -i -c 'nvm install node; \
            nvm use node; \
-           npm install -g typescript angular-cli live-server vorlon surge;'
+           npm install -g typescript @angular/cli live-server vorlon surge;'
 
 timerData "POST-NODE"
 
