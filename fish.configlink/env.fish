d3a9e76cc7d6924f3aa20ea76f986cd9e3567599
Shane Liesegang <shane@techie.net>
2020-12-30 21:55:49 +0200

    whoops

--- fish.configlink/env.fish
+++ fish.configlink/env.fish
@@ -1,31 +1,31 @@
 # unicode. always, unicode
-set LANG en_US.UTF-8
-set LANGUAGE $LANG
-set LC_ALL $LANG
+set -x LANG en_US.UTF-8
+set -x LANGUAGE $LANG
+set -x LC_ALL $LANG
 
 source $HOME/.config/fish/path.fish
 
 # Preferred editor for local and remote sessions
-set EDITOR "vim"
+set -x EDITOR "vim"
 if test -f "$HOME/bin/sublime-wait"
-  set VISUAL "$HOME/bin/sublime-wait"
+  set -x VISUAL "$HOME/bin/sublime-wait"
 end
 
 ## homebrew setup
 # don't phone home
-set HOMEBREW_NO_ANALYTICS 1
+set -x HOMEBREW_NO_ANALYTICS 1
 # relax
-set HOMEBREW_AUTO_UPDATE_SECS "86400"
+set -x HOMEBREW_AUTO_UPDATE_SECS "86400"
 # don't build from scratch if a bottle download fails
-set HOMEBREW_NO_BOTTLE_SOURCE_FALLBACK 1
+set -x HOMEBREW_NO_BOTTLE_SOURCE_FALLBACK 1
 
 ## python setup
 # since we have our own plans for the prompt
-set VIRTUAL_ENV_DISABLE_PROMPT 1
+set -x VIRTUAL_ENV_DISABLE_PROMPT 1
 # pipenv's default behavior is silly
-set PIPENV_VENV_IN_PROJECT 1
+set -x PIPENV_VENV_IN_PROJECT 1
 
 ## purty colors
-set LSCOLORS exfxcxdxbxegedabagacad
+set -x LSCOLORS exfxcxdxbxegedabagacad
 # set LSCOLORS GxFxCxDxBxegedabagaced
-set CLICOLOR 1
+set -x CLICOLOR 1
