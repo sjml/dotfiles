0956428ef27f5a6f63281b2abd08a48f3eb4f764
Shane Liesegang <shane@techie.net>
2021-01-24 09:32:51 +0200

    fixing rtab for nested paths with spaces

--- fish.configlink/functions/rtab.fish
+++ fish.configlink/functions/rtab.fish
@@ -1,23 +1,12 @@
-function rtab
-  set dir $PWD
-
-  # replace $HOME with ~
-  set dir (string replace -r "^$HOME" \~ $dir)
-
+function shorten_path -a full_directory replace_tilde expand_final
   set fullPath ""
   set result ""
-  set tree (string split '/' $dir)
-  if test $tree[1] = "~"
-    set tree $tree[2..-1]
-    set result "~"
-    set fullPath "~"
-  end
+  set tree (string split --no-empty '/' $full_directory)
+
   for dir in $tree;
-    if test -z $dir
-      continue
-    end
-    if test (count $tree) -eq 1;
+    if test $expand_final -eq 1; and test (count $tree) -eq 1;
       set result $result/$dir
+      set fullPath $fullPath/$dir
       set tree $tree[2..-1]
       break
     end
@@ -25,16 +14,33 @@ function rtab
     set part ""
     for c in (string split '' $dir);
       set part $part$c
-      set options (string split ' ' (eval echo "$fullPath/$part*/"))
 
+      set -e options
+      for p in $fullPath/$part*;
+        set -a options $p
+      end
       if test (count $options) -eq 1;
         break
       end
     end
-    set tree $tree[2..-1]
-    set fullPath $fullPath/$dir
+
     set result $result/$part
+    set fullPath $fullPath/$dir
+    set tree $tree[2..-1]
+  end
+
+  if test $replace_tilde -eq 1; and string match -r "^$HOME" $fullPath > /dev/null;
+    set short_home (shorten_path $HOME 0 0)
+    set result (string replace $short_home "~" $result)
   end
 
   echo $result
+end
+
+function rtab
+  if test $PWD = $HOME
+    shorten_path $PWD 1 0
+  else
+    shorten_path $PWD 1 1
+  end
 end
