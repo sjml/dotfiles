3beaa72c1266d73551327e7575f820fbb189252b
Shane Liesegang <shane@techie.net>
2022-04-20 20:16:47 +0300

    ok, granted ASDF kind of sucks, but somehow homebrew makes it suck more? this still doesn't work without a patch to ASDF and I'm starting to think I should just bail on the whole thing

--- fish.configlink/path.fish
+++ fish.configlink/path.fish
@@ -44,6 +44,7 @@ if test -f /usr/local/opt/asdf/asdf.fish;
   source /usr/local/opt/asdf/asdf.fish
   set haveASDF 1
 else if test -f /opt/homebrew/opt/asdf/asdf.fish
+  set -x ASDF_DIR /opt/homebrew/opt/asdf/libexec
   source /opt/homebrew/opt/asdf/asdf.fish
   set haveASDF 1
 else if test -f $HOME/.asdf/asdf.fish
