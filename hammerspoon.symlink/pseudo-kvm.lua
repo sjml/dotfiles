9c8179cf1227790f820a14399d7726665f3406a9
Shane Liesegang <shane@techie.net>
2023-07-08 00:01:18 -0400

    stop overengineering

--- hammerspoon.symlink/pseudo-kvm.lua
+++ hammerspoon.symlink/pseudo-kvm.lua
@@ -10,6 +10,10 @@ local DOCKING_STATION_PRODUCT = 0x5500
 local USB_SWITCH_VENDOR = 0x1a40
 local USB_SWITCH_PRODUCT = 0x0101
 
+local M1DDC_PATH = "/opt/homebrew/bin/m1ddc"
+local MAC_INPUT_ID = "15"
+local WIN_INPUT_ID = "17"
+
 
 -- look for the the docking station for my Mac
 local function isDocked()
@@ -27,11 +31,11 @@ local function usbWatcherCallback(data)
   end
   if data["vendorID"] == USB_SWITCH_VENDOR and data["productID"] == USB_SWITCH_PRODUCT then
     if data["eventType"] == "added" then
-      -- change to DisplayPort 1 (input 15)
-      local output, status, _type, rc = hs.execute("/opt/homebrew/bin/m1ddc set input 15")
+      -- switch showed up on this machine; move to our input
+      local output, status, _type, rc = hs.execute(M1DDC_PATH .. " set input " .. MAC_INPUT_ID)
     elseif data["eventType"] == "removed" then
-      -- change to HDMI 1 (input 17)
-      local output, status, _type, rc = hs.execute("/opt/homebrew/bin/m1ddc set input 17")
+      -- switch was removed from this machine; move to other input
+      local output, status, _type, rc = hs.execute(M1DDC_PATH .. " set input " .. WIN_INPUT_ID)
     end
   end
 end
