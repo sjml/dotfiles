b01029b20d6363d49399cb98d8f788ce7de2b1ed
Shane Liesegang <shane@techie.net>
2026-03-30 11:10:05 -0400

    feed an optional locale to transcribe

--- fish.configlink/functions/transcribe.fish
+++ fish.configlink/functions/transcribe.fish
@@ -1,6 +1,22 @@
 function transcribe
+		set locale ""
+
+		while test (count $argv) -gt 0
+				switch $argv[1]
+						case '-l' '--locale'
+								if test (count $argv) -lt 2
+										echo "Error: --locale requires a value"
+										return 1
+								end
+								set locale $argv[2]
+								set -e argv[1..2]
+						case '*'
+								break
+				end
+		end
+
 		if test (count $argv) -eq 0
-				echo "Usage: transcribe <file1> [file2 ...]"
+				echo "Usage: transcribe [-l <locale>] <file1> [file2 ...]"
 				return 1
 		end
 
@@ -14,6 +30,11 @@ function transcribe
 				set out "$base".srt
 
 				echo "Processing $file → $out"
-				yap transcribe --srt -o "$out" "$file"
+
+				if test -n "$locale"
+						yap transcribe --srt -o "$out" -l "$locale" "$file"
+				else
+						yap transcribe --srt -o "$out" "$file"
+				end
 		end
 end
